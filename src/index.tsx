cc83c893942387e1bafba4cb66954ebcd2c9e737
The Color Blurple <turtlenerd603@gmail.com>
2024-07-29 13:25:54 -0400

    Fix URL Collision

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './style.scss';
 import Home from "./pages/home/home.tsx";
@@ -18,7 +18,6 @@ function App() {
 				<Route path="/news" element={<News />} />
 				<Route path="/mods" element={<Mods />} />
 				<Route path="/account" element={<Home />} />
-				<Route path="*" element={<NoMatch />} />
 			</Routes>
 		</Router>
 	);
@@ -41,21 +40,4 @@ function Header() {
 
 		</nav>
 	)
-}
-
-function NoMatch() {
-	return (
-		<div className="page">
-			<h1>Page doesnt exist!</h1>
-			<p>
-				<i>
-				You find yourself in an empty room. There is nothing here but a few chairs and a table with food left out on it.
-				Dusty hardwood floors, grimy paper thin walls, shattered glass from the windows nearby that somehow made its way across the room.
-				The history of this place, gone in what feels like an instant, but you can see the beauty in the missing and gone.
-				<br/><br/>
-				You turn around and see yourself out. <b><Link className="homeButton" to="/">Lets go home</Link></b>.
-				</i>
-			</p>
-		</div>
-	);
 }
